6b8f3c882a14ced88bcc1b8c63ada77a6f8af3e7
redukr <red.ukr@gmail.com>
2025-11-19 16:27:08 +0200

    0.1.2

--- app/build.bat
+++ app/build.bat
@@ -7,6 +7,7 @@ echo ===============================
 
 echo.
 echo --- Очистка старих збірок ---
+echo.
 
 IF EXIST dist (
     echo Видаляю dist...
@@ -27,7 +28,7 @@ echo.
 echo --- Запуск PyInstaller ---
 echo.
 
-"..\python310\python.exe" -m PyInstaller CardGenerator.spec
+"C:\Program Files\python310\python.exe" -m PyInstaller CardGenerator.spec
 
 echo.
 echo ===============================
